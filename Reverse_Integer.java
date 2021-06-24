145f3eaef52432175eddfc6f44845d72030a70b3
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-06-24 17:28:09 +0700

    Create Reverse_Integer.java
    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
    
    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
    
     
    
    Example 1:
    
    Input: x = 123
    Output: 321
    
    Example 2:
    
    Input: x = -123
    Output: -321
    
    Example 3:
    
    Input: x = 120
    Output: 21
    
    Example 4:
    
    Input: x = 0
    Output: 0
    
     
    
    Constraints:
    
        -231 <= x <= 231 - 1

---
+++ Reverse_Integer.java
@@ -0,0 +1,26 @@
+class Solution {
+  public int reverse(int x) {
+    //Set b to long to contain all the possibilities
+    long b = 0;               
+    while (x != 0) {
+      b = b * 10 + x % 10;
+      x = x / 10l
+      }
+    if (b < Integer.MIN_VALUE || b > Integer.MAX_VALUE) {
+      return 0;
+    } else {
+      return b;
+    }
+  }
+}
+--------------------------------------------------------------
+public int reverse(int x) {
+  String ans = x < 0 ? new StringBuilder(String.valueOf(-x)).append("-").reverse().toString()
+    : new StringBuilder(String.valueOf(x)).reverse().toString();
+  try {
+    return Integer.parseInt(ans);
+  } catch (Exception e) {
+    return 0;
+  }
+}
+      
