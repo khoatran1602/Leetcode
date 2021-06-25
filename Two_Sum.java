0b02191fdaca99fe07431674c3c362af5766b660
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-06-25 18:01:04 +0700

    Create Two_Sum.java
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]
    
     
    
    Constraints:
    
        2 <= nums.length <= 104
        -109 <= nums[i] <= 109
        -109 <= target <= 109
        Only one valid answer exists.
    Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?

---
+++ Two_Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      Map<Integer, Integer> map = new Map<>();
+      for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+      int foundNumber = target - nums[i];
+      if (map.containsKey(foundNumber) && map.get(nums[i]) != i) {
+        return new int[] (i, map.get(nums[i]));
+      }
+      throw new IllegalArgumentException("No two sum solution");
+    }
+}
