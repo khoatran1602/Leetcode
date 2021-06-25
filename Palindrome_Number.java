4f4f7311c82225d719cc5ce4145b3ad1b6b08650
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-06-25 18:58:34 +0700

    Create Palindrome_Number.java
    Given an integer x, return true if x is palindrome integer.
    
    An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
    
     
    
    Example 1:
    
    Input: x = 121
    Output: true
    
    Example 2:
    
    Input: x = -121
    Output: false
    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
    
    Example 3:
    
    Input: x = 10
    Output: false
    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
    
    Example 4:
    
    Input: x = -101
    Output: false
    
     
    
    Constraints:
    
        -231 <= x <= 231 - 1
    Follow up: Could you solve it without converting the integer to a string?

---
+++ Palindrome_Number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       if ( x < 0) {
+            return false;
+        }
+      //Assign x = z so when we divide x by 10 later then we still have one more number to compare.
+        int z = x;
+        int y = 0;
+        while (x != 0) {
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+        if (z == 0 || z == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+-----------------------------------------------------------------------------------------------------
+class Solution {
+  public boolean isPalindrome(int x) {
+    String y = String.valueOf(x);
+    int start = 0;
+    int end = y.length() - 1;
+    // We don't have to have "=" (start <= end) because when there is one number, it is palindrome.
+    while (start < end) {
+      if (y.charAt(start++) != y.charAt(end--)) return false;
+    }
+    return true;
+  }
+}
