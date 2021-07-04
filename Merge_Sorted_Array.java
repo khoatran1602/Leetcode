d882b861d9e3d798c4f9a3c74cff792418f0d3e4
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-07-04 20:06:04 +0700

    Create Merge_Sorted_Array.java
    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead, be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
    
    Example 1:
    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
    Output: [1,2,2,3,5,6]
    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
    
    Example 2:
    Input: nums1 = [1], m = 1, nums2 = [], n = 0
    Output: [1]
    Explanation: The arrays we are merging are [1] and [].
    The result of the merge is [1].
    
    Example 3:
    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
    Output: [1]
    Explanation: The arrays we are merging are [] and [1].
    The result of the merge is [1].
    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
    
    Constraints:
        nums1.length == m + n
        nums2.length == n
        0 <= m, n <= 200
        1 <= m + n <= 200
        -109 <= nums1[i], nums2[j] <= 109
    
    Follow up: Can you come up with an algorithm that runs in O(m + n) time?

---
+++ Merge_Sorted_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int curr1 = m -1;
+    int curr2 = n -1;
+    int start = m + n - 1;
+    while (curr1 >= 0 && curr2 >= 0) {
+      if (nums1[curr1] > nums2[curr2]) {
+        nums1[start--] = nums1[curr1--];
+      } else {
+        nums1[start--] = nums2[curr2--];
+      }
+    }
+    if(curr1 <= 0) {
+      while(curr2 >= 0) nums1[start--] = nums2[curr2--];
+    }
+  } 
+}
