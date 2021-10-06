fabc4ea04835472e6fe4b13cedef8d1e0634e962
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-10-06 15:54:26 +0700

    Update E1_Two_Sum.java

--- E1_Two_Sum.java
+++ E1_Two_Sum.java
@@ -7,7 +7,7 @@ class Solution {
     for (int i = 0; i < nums.length; i++) {
       int foundNumber = target - nums[i];
       if (map.containsKey(foundNumber) && map.get(nums[i]) != i) {
-        return new int[] (i, map.get(nums[i]));
+        return new int[] {i, map.get(nums[i])};
       }
       throw new IllegalArgumentException("No two sum solution");
     }
