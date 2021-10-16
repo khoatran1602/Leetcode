3ca21347178d7173727c45feadfaba5f4d4f778d
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-10-16 16:17:37 +0700

    Update E191_Number_of_1_Bits.java
    Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
    
    Note:
    
    Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
    
    Example 1:
    Input: n = 00000000000000000000000000001011
    Output: 3
    Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
    
    Example 2:
    Input: n = 00000000000000000000000010000000
    Output: 1
    Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
    
    Example 3:
    Input: n = 11111111111111111111111111111101
    Output: 31
    Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.

--- E191_Number_of_1_Bits.java
+++ E191_Number_of_1_Bits.java
@@ -10,3 +10,16 @@ public class Solution {
         return count;
     }
 }
+======================================================
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count  = 0;
+        int num = 1;
+        for (int i = 0; i < 32; i++) {
+            int check = num << i;
+            if ((n & check) == check) count++;
+        }
+        return count;
+    }
+}
