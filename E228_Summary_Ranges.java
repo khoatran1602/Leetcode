d3aa5655d2e4cabc4513868c448053a6091206fd
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-10-21 17:49:45 +0700

    Create E228_Summary_Ranges.java
    You are given a sorted unique integer array nums.
    
    Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
    
    Each range [a,b] in the list should be output as:
        "a->b" if a != b
        "a" if a == b
    
    Example 1:
    Input: nums = [0,1,2,4,5,7]
    Output: ["0->2","4->5","7"]
    Explanation: The ranges are:
    [0,2] --> "0->2"
    [4,5] --> "4->5"
    [7,7] --> "7"
    
    Example 2:
    Input: nums = [0,2,3,4,6,8,9]
    Output: ["0","2->4","6","8->9"]
    Explanation: The ranges are:
    [0,0] --> "0"
    [2,4] --> "2->4"
    [6,6] --> "6"
    [8,9] --> "8->9"
    
    Example 3:
    Input: nums = []
    Output: []
    
    Example 4:
    Input: nums = [-1]
    Output: ["-1"]
    
    Example 5:
    Input: nums = [0]
    Output: ["0"]

---
+++ E228_Summary_Ranges.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList<>();
+        if (nums.length == 0) return list;
+        if (nums.length == 1) {
+            list.add(nums[0] + "");
+        }
+        int length = nums.length - 1;
+        int first = 0;
+        int last = 0;
+        for (int i = 0; i < length; i++) {
+            if ((nums[i] + 1) == nums[i+1]) {
+                last++;
+            } else {
+                if (first != last) {
+                    list.add(nums[first] + "->" + nums[last]);
+                    last++;
+                    first = last;
+                } else {
+                    list.add(nums[last] + "");
+                    last++;
+                    first = last;
+                }
+            }
+            if (nums[i+1] == nums[length] && nums[i+1] != (nums[i] + 1)) list.add(nums[length] + "");
+            else if (nums[i+1] == nums[length] && nums[i+1] == (nums[i] + 1)) {
+                list.add(nums[first] + "->" + nums[last]);
+            }
+        }
+        return list;
+    }
+}
