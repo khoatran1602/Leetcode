11e44276662383dcaa7dd6991504efe196c914c6
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-11-03 16:41:25 +0700

    Create E705_Design_HashSet.java
    Design a HashSet without using any built-in hash table libraries.
    
    Implement MyHashSet class:
        void add(key) Inserts the value key into the HashSet.
        bool contains(key) Returns whether the value key exists in the HashSet or not.
        void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
     
    Example 1:
    Input
    ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
    [[], [1], [2], [1], [3], [2], [2], [2], [2]]
    Output
    [null, null, null, true, false, null, true, null, false]
    
    Explanation
    MyHashSet myHashSet = new MyHashSet();
    myHashSet.add(1);      // set = [1]
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(1); // return True
    myHashSet.contains(3); // return False, (not found)
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(2); // return True
    myHashSet.remove(2);   // set = [1]
    myHashSet.contains(2); // return False, (already removed)
    
    Constraints:
        0 <= key <= 106
        At most 104 calls will be made to add, remove, and contains.

---
+++ E705_Design_HashSet.java
@@ -0,0 +1,45 @@
+class MyHashSet {
+    
+    private final ArrayList<Integer> list = new ArrayList<>();
+
+    public MyHashSet() {
+    }
+    
+    public void add(int key) {
+        if (list.size() == 0) {
+            list.add(key);
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.contains(key)) {
+                    break;
+                } else {
+                    list.add(key);
+                }
+            }
+        }
+    }
+    
+    public void remove(int key) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(key)) list.remove(i);
+        }
+    }
+    
+    public boolean contains(int key) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+=============================================================================
