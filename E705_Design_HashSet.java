2bbbb3df3546b3278afe0afba5e987fd759f82b9
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2021-11-03 18:38:19 +0700

    Update E705_Design_HashSet.java
    Design a HashSet without using any built-in hash table libraries.
    
    Implement MyHashSet class:
        void add(key) Inserts the value key into the HashSet.
        bool contains(key) Returns whether the value key exists in the HashSet or not.
        void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
     
    Example 1:
    Input
    ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
    [[], [1], [2], [1], [3], [2], [2], [2], [2]]
    Output
    [null, null, null, true, false, null, true, null, false]
    
    Explanation
    MyHashSet myHashSet = new MyHashSet();
    myHashSet.add(1);      // set = [1]
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(1); // return True
    myHashSet.contains(3); // return False, (not found)
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(2); // return True
    myHashSet.remove(2);   // set = [1]
    myHashSet.contains(2); // return False, (already removed)
    
    Constraints:
        0 <= key <= 106
        At most 104 calls will be made to add, remove, and contains.

--- E705_Design_HashSet.java
+++ E705_Design_HashSet.java
@@ -43,3 +43,19 @@ class MyHashSet {
  * boolean param_3 = obj.contains(key);
  */
 =============================================================================
+private final boolean arr[];
+public MyHashSet() {
+	arr = new boolean[1000001];
+}
+
+public void add(int key) {
+	arr[key] = true;
+}
+
+public void remove(int key) {
+	arr[key] = false;
+}
+
+public boolean contains(int key) {
+	return arr[key];
+}
