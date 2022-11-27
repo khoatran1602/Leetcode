c070c82bdbca4bcbdc74f62855f5c7a445de943d
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2022-11-28 02:10:01 +0700

    Create E278.First Bad Version.java
    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
    
    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
    
    You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
    
     
    
    Example 1:
    
    Input: n = 5, bad = 4
    Output: 4
    Explanation:
    call isBadVersion(3) -> false
    call isBadVersion(5) -> true
    call isBadVersion(4) -> true
    Then 4 is the first bad version.
    Example 2:
    
    Input: n = 1, bad = 1
    Output: 1
     
    
    Constraints:
    
    1 <= bad <= n <= 2^31 - 1

---
+++ E278.First Bad Version.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int end = n;
+        int start = 0;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (isBadVersion(start)) {
+            return start;
+        }
+        return end;
+    }
+}
