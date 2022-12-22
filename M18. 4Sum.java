b022a5aeecd51a3f59267e709b4a98d8b0658fbf
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2022-12-22 23:48:06 +0700

    Create M18. 4Sum.java
    Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
    
    0 <= a, b, c, d < n
    a, b, c, and d are distinct.
    nums[a] + nums[b] + nums[c] + nums[d] == target
    You may return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [1,0,-1,0,-2,2], target = 0
    Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
    Example 2:
    
    Input: nums = [2,2,2,2,2], target = 8
    Output: [[2,2,2,2]]
     
    
    Constraints:
    
    1 <= nums.length <= 200
    -10^9 <= nums[i] <= 10^9
    -10^9 <= target <= 10^9

---
+++ M18. 4Sum.java
@@ -0,0 +1,51 @@
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+                int end = nums.length - 1;
+                while (i < end - 2) {
+                    int index1 = i + 1;
+                    int index2 = end - 1;
+                    int s = nums[i] + nums[end];
+                    if (s > target && nums[i] > 0 || s < target && nums[end] < 0) {
+
+                        break;
+                    }
+
+                    while (index1 < index2) {
+
+                        int sum = nums[index1] + nums[index2] + nums[i] + nums[end];
+                        if (sum == target) {
+                            List<Integer> list = new ArrayList<Integer>();
+                            list.add(nums[i]);
+                            list.add(nums[index1]);
+                            list.add(nums[index2]);
+                            list.add(nums[end]);
+
+                            // set.add(list);
+                            res.add(list);
+
+                            while (nums[index2] == nums[index2 - 1] && index2 > index1) index2--;
+                            ;
+                            index1++;
+                            index2--;
+                        } else if (sum < target) {
+                            index1++;
+                        } else {
+                            index2--;
+                        }
+
+
+                    }
+                    while (nums[end] == nums[end - 1] && i < end - 2) end--;
+                    end--;
+
+                }
+            }
+        }
+
+        return res;
+    }
+}
