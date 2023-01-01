6e1b7489b337ac0c3d7034a5d405e30a8d7e3f16
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-01-01 18:06:30 +0700

    Update E507. Perfect Number.java
    A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
    
    Given an integer n, return true if n is a perfect number, otherwise return false.
    
     
    
    Example 1:
    
    Input: num = 28
    Output: true
    Explanation: 28 = 1 + 2 + 4 + 7 + 14
    1, 2, 4, 7, and 14 are all divisors of 28.
    Example 2:
    
    Input: num = 7
    Output: false
     
    
    Constraints:
    
    1 <= num <= 10^8

--- E507. Perfect Number.java
+++ E507. Perfect Number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkPerfectNumber(int num) {
         int res = 0;
         if (num % 2 != 0) return false;
-        for (int i = 1; i < num / 2; i++) {
+        for (int i = 1; i <= num / 2; i++) {
             if (num % i == 0) {
                 res = res + i;
             }
