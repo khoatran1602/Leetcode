9189c40d7b7979426da2e41f04c4832746bd9ca8
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-02-01 00:11:56 +0700

    Update E872. Leaf-Similar Trees.java
    Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
    
    
    
    For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
    
    Two binary trees are considered leaf-similar if their leaf value sequence is the same.
    
    Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
    
     
    
    Example 1:
    
    
    Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
    Output: true
    Example 2:
    
    
    Input: root1 = [1,2,3], root2 = [1,3,2]
    Output: false
     
    
    Constraints:
    
    The number of nodes in each tree will be in the range [1, 200].
    Both of the given trees will have values in the range [0, 200].

--- E872. Leaf-Similar Trees.java
+++ E872. Leaf-Similar Trees.java
@@ -1,15 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isUnivalTree(TreeNode root) {
-        // return true if root is null
-        if (root == null) return true;
-        // return result of checkBool method
-        return checkBool(root, root.val);
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        fillList(root1, list1);
+        fillList(root2, list2);
+        return list1.equals(list2);
     }
 
-    public boolean checkBool(TreeNode root, int val) {
-        // return true if root is null
-        if (root == null) return true;
-        // return true if the value of root is equal to given value and both left and right subtrees are unival trees
-        return root.val == val && checkBool(root.left, val) && checkBool(root.right, val);
+    public void fillList(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+        fillList(root.left, list);
+        fillList(root.right, list);
     }
 }
