7fd239c13040eae16b751fdc1b01d0e4250c2deb
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-02-02 00:56:22 +0700

    Update E993. Cousins in Binary Tree.java
    Given the root of a binary tree with unique values and the values of two different nodes of the tree x and y, return true if the nodes corresponding to the values x and y in the tree are cousins, or false otherwise.
    
    Two nodes of a binary tree are cousins if they have the same depth with different parents.
    
    Note that in a binary tree, the root node is at the depth 0, and children of each depth k node are at the depth k + 1.
    
     
    
    Example 1:
    
    
    Input: root = [1,2,3,4], x = 4, y = 3
    Output: false
    Example 2:
    
    
    Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
    Output: true
    Example 3:
    
    
    Input: root = [1,2,3,null,4], x = 2, y = 3
    Output: false
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [2, 100].
    1 <= Node.val <= 100
    Each node has a unique value.
    x != y
    x and y are exist in the tree.

--- E993. Cousins in Binary Tree.java
+++ E993. Cousins in Binary Tree.java
@@ -1,25 +1,30 @@
 class Solution {
+    // Entry point method to check if two nodes in a binary tree are cousins
     public boolean isCousins(TreeNode root, int x, int y) {
-        if (root == null) return true;
-        int depth = 0;
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int second = 0;
-        findDepth(root, 0, list, x);
-        findDepth(root, 0, list, y);
-        if (list.get(1) == 1) return false;
-        if (list.get(0) == list.get(1)) return true;
-        return false;
+        // Variables to store parent and depth of node x and y
+        int[] parentX = {0};
+        int[] parentY = {0};
+        int[] depthX = {-1};
+        int[] depthY = {-1};
+        // Call helper method to find the depth and parent of node x
+        findDepth(root, null, parentX, depthX, 0, x);
+        // Call helper method to find the depth and parent of node y
+        findDepth(root, null, parentY, depthY, 0, y);
+        // Return true if both nodes have the same depth and different parents
+        return depthX[0] == depthY[0] && parentX[0] != parentY[0];
     }
 
-    public void findDepth(TreeNode root, int depth, List<Integer> list, int num) {
+    // Helper method to find depth and parent of a node in a binary tree
+    public void findDepth(TreeNode root, TreeNode parent, int[] parentNode, int[] depthNode, int depth, int num) {
+        // Base case: if root is null, return
         if (root == null) return;
+        // If root's value is equal to num, update parentNode and depthNode
         if (root.val == num) {
-            list.add(depth);
-            list.add(depth + 1);
+            depthNode[0] = depth;
+            parentNode[0] = parent != null ? parent.val : 0;
         }
-        depth = depth + 1;
-        findDepth(root.left, depth, list, num);
-        findDepth(root.right, depth, list, num);
+        // Recursively call the helper method on left and right children
+        findDepth(root.left, root, parentNode, depthNode, depth + 1, num);
+        findDepth(root.right, root, parentNode, depthNode, depth + 1, num);
     }
 }
