6c2acc15e268e3479eb825dd07ad2789bb438376
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-02-20 00:01:47 +0700

    Update M75. Sort Colors.java
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
    
    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
    
    You must solve this problem without using the library's sort function.
    
     
    
    Example 1:
    
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]
    Example 2:
    
    Input: nums = [2,0,1]
    Output: [0,1,2]
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 300
    nums[i] is either 0, 1, or 2.

--- M75. Sort Colors.java
+++ M75. Sort Colors.java
@@ -29,3 +29,19 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {  // if current element is 0
+                nums[i] = nums[left];  // move it to the left
+                nums[left++] = 0;  // and move the left pointer to the right
+            } else if (nums[i] == 2) {  // if current element is 2
+                nums[i--] = nums[right];  // move it to the right
+                nums[right--] = 2;  // and move the right pointer to the left
+            }
+        }
+    }
+}
+
