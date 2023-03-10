1d91ff722ce0e463bbaf06deae6e91585d3c9f80
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-03-10 22:16:09 +0700

    Create
    You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
    
    If isLefti == 1, then childi is the left child of parenti.
    If isLefti == 0, then childi is the right child of parenti.
    Construct the binary tree described by descriptions and return its root.
    
    The test cases will be generated such that the binary tree is valid.
    
     
    
    Example 1:
    
    
    Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
    Output: [50,20,80,15,17,19]
    Explanation: The root node is the node with value 50 since it has no parent.
    The resulting binary tree is shown in the diagram.
    Example 2:
    
    
    Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
    Output: [1,2,null,null,3,4]
    Explanation: The root node is the node with value 1 since it has no parent.
    The resulting binary tree is shown in the diagram.
     
    
    Constraints:
    
    1 <= descriptions.length <= 10^4
    descriptions[i].length == 3
    1 <= parenti, childi <= 10^5
    0 <= isLefti <= 1
    The binary tree described by descriptions is valid.

---
+++ M2196. Create Binary Tree From Descriptions.java
@@ -0,0 +1,40 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        // Create a map to store nodes and their parent-child relationships
+        HashMap<Integer, TreeNode> map = new HashMap<>();
+        // Create a set to keep track of all the child nodes
+        Set<Integer> children = new HashSet<>();
+        
+        // Loop through each description in the input array
+        for (int[] arr : descriptions) {
+            // Extract parent, child, and whether child is left or right from the current description
+            int parent = arr[0], child = arr[1], isLeft = arr[2];
+            // Add child to the set of children
+            children.add(child);
+            // Get the parent node from the map, or create a new node if it doesn't exist
+            TreeNode node = map.getOrDefault(parent, new TreeNode(parent));
+            // Set the child node as the left or right child of the parent node, depending on isLeft
+            if (isLeft == 1) {
+                node.left = map.getOrDefault(child, new TreeNode(child));
+                map.put(child, node.left);
+            } else {
+                node.right = map.getOrDefault(child, new TreeNode(child));
+                map.put(child, node.right);
+            }
+            // Update the parent node in the map
+            map.put(parent, node);
+        }
+        
+        // Find the root node of the tree by looking for a node that is not a child of any other node
+        int root = -1;
+        for (int [] arr: descriptions) {
+            if (!children.contains(arr[0])) {
+                root = arr[0];
+                break;
+            }
+        }
+        
+        // Return the root node, or null if it doesn't exist
+        return map.getOrDefault(root, null);
+    }
+}
