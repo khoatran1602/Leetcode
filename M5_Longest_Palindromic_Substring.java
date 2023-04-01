f87c3800a51ea47f7ebb4c19a304c0119d5cb65c
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2023-04-01 20:44:12 +0700

    Update M5_Longest_Palindromic_Substring.java
    Given a string s, return the longest 
    palindromic
     
    substring
     in s.
    
     
    
    Example 1:
    
    Input: s = "babad"
    Output: "bab"
    Explanation: "aba" is also a valid answer.
    Example 2:
    
    Input: s = "cbbd"
    Output: "bb"
     
    
    Constraints:
    
    1 <= s.length <= 1000
    s consist of only digits and English letters.

--- M5_Longest_Palindromic_Substring.java
+++ M5_Longest_Palindromic_Substring.java
@@ -1,24 +1,25 @@
 class Solution {
   public String longestPalindrome(String s) {
-    String res = "";
-    int currentLength = 0;
-    for(int i = 0; i < s.length(); i++) {
-      if(isPalindrome(s, i - currLength - 1, i)) {
-        res = s.substring(i - currentLength - 1, i + 1);
-        currentLength = currentLength + 2;
+    String longestPalindrome = ""; // Initialize an empty string to store the longest palindrome found
+    int maxLength = 0; // Initialize a variable to store the length of the longest palindrome found
+    for(int i = 0; i < s.length(); i++) { // Iterate over each character in the string
+      if(isPalindrome(s, i - maxLength - 1, i)) { // Check if a palindrome centered at i with length maxLength+1 exists
+        longestPalindrome = s.substring(i - maxLength - 1, i + 1); // Update longestPalindrome to the new palindrome
+        maxLength += 2; // Update maxLength to reflect the new longest palindrome found
       }
-      else if(isPalindrome(s, i - currentLength, i)){
-        res = s.substring(i - currentLength, i + 1);
-        currentLength = currentLength + 1;
+      else if(isPalindrome(s, i - maxLength, i)){ // Check if a palindrome centered at i with length maxLength exists
+        longestPalindrome = s.substring(i - maxLength, i + 1); // Update longestPalindrome to the new palindrome
+        maxLength += 1; // Update maxLength to reflect the new longest palindrome found
       }
     }
-    return res;
+    return longestPalindrome; // Return the longest palindrome found
   }
   
-public boolean isPalindrome(String s, int begin, int end) {
-  if (begin < 0) return false;
-  while (begin < end) {
-    if(s.charAt(begin++) != s.charAt(end--)) return false;
+  public boolean isPalindrome(String s, int start, int end) {
+    if (start < 0) return false; // If start index is less than 0, then it is out of bounds
+    while (start < end) { // Check if the string is a palindrome
+      if(s.charAt(start++) != s.charAt(end--)) return false; // If any characters don't match, it's not a palindrome
+    }
+    return true; // If the loop completes without returning false, it's a palindrome
   }
-  return true;
 }
