151f8e4156f152a7d4443315a122067fa5f5b719
unknown <trankhoa758@gmail.com>
2024-12-30 22:31:31 +0700

    feat: feat: add new solutions for various problems and remove outdated files

--- E1_Two_Sum.java
+++ E1_Two_Sum.java
@@ -1,14 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-      Map<Integer, Integer> map = new Map<>();
-      for (int i = 0; i < nums.length; i++) {
-        map.put(nums[i], i);
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      map.put(nums[i], i);
     }
     for (int i = 0; i < nums.length; i++) {
       int foundNumber = target - nums[i];
-      if (map.containsKey(foundNumber) && map.get(nums[i]) != i) {
-        return new int[] {i, map.get(nums[i])};
+      if (map.containsKey(foundNumber) && map.get(foundNumber) != i) {
+        return new int[] { i, map.get(foundNumber) };
       }
-      throw new IllegalArgumentException("No two sum solution");
     }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+  
+  public static void main(String[] args) {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    int[] result = twoSum(nums, target);
+    System.out.println(result[0] + " " + result[1]);
+  }
 }
---
+++ E643_Maximum_Average_SubArray_I.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        // Placeholder return value
+        return 0.0;
+    }
+
+    public static void 
+}
