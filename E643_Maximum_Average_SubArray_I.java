ae098fc91ad28a2b715413ead087b35c277005f8
unknown <trankhoa758@gmail.com>
2024-12-30 22:55:43 +0700

    chore: update

--- E643_Maximum_Average_SubArray_I.java
+++ E643_Maximum_Average_SubArray_I.java
@@ -1,8 +1,21 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        // Placeholder return value
-        return 0.0;
+        double sum = 0.0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double max = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+        return max / k;
     }
 
-    public static void 
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 12, -5, -6, 50, 3};
+        int k = 4;
+        System.out.println(sol.findMaxAverage(nums, k));
+    }
 }
